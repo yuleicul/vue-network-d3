dc28a0a3a6fb2a2816d4d038eb59ab316dfd3dba
ChenCyl <programming2@163.com>
2019-05-23 21:14:11 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Network msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Network from './components/Network.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Network
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
---
+++ src/components/Network.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <button id="reset">reset</button>
+
+    <!-- 这个到时候放在 view 上 写代码的时候放在这里方便 -->
+    <div class="svgContainer">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        :width="size.w"
+        :height="size.h"
+      >
+        <!-- node and node-text -->
+        <g>
+          <g >
+            <circle></circle>
+            <text></text>
+          </g>
+          <g></g>
+        </g>
+
+        <!-- links and link-text -->
+        <g>
+          <g></g>
+          <g></g>
+        </g>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as d3 from "d3"; // 先这样导入 最后优化的时候要按需导入
+
+export default {
+  name: "network"
+};
+</script>
+
+
+
+<style scoped>
+</style>
