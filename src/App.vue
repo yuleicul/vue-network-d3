ca4131fcaebf08bc7ce47d4f05a6f3fe1dc31e05
ChenCyl <programming2@163.com>
2019-05-29 20:58:31 +0800

    feat: search

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,24 @@
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <Network msg="Welcome to Your Vue.js App"/> -->
     <div id="modeSwitch">
-    <input type="checkbox" name=""  v-model="mode" true-value='dark' false-value='light'> dark
+      <input type="checkbox" name v-model="mode" true-value="dark" false-value="light"> dark
     </div>
-    <network v-if="canRender" :nodes="nodes" :links="links" :showLinkText="showLinkText"
-    :mode="mode"></network>
+    <input type="text" v-model="queryWords">
+    <button @click="startQuery(queryWords)">搜索</button>
+    <network
+      v-if="canRender"
+      :nodeList="nodes"
+      :linkList="links"
+      :showLinkText="showLinkText"
+      :mode="mode"
+    ></network>
   </div>
 </template>
 
 <script>
 import Network from "./components/Network.vue";
-import axios from 'axios'
+import jsonpFetch from "./lib/jsonpFetch.js";
+import axios from "axios";
 
 export default {
   name: "app",
@@ -24,18 +32,125 @@ export default {
       showLinkText: false,
       nodes: [],
       links: [],
+      nodeIds: [], // 用于判断重复节点
       canRender: false,
-      mode: 'light' // dark or light
+      mode: "light", // dark or light
+      queryWords: ""
     };
   },
   created() {
-    axios.get('/example.json')
-      .then(response => {
-        this.nodes = response.data.nodes
-        this.links = response.data.links
-        this.canRender = true
-      }).catch(err=>console.log(err))
+    // this.getFirstLayerNode("阿里云").then(res => {
+    //   let promiseList = [];
+    //   res.forEach(node => {
+    //     promiseList.push(this.getFirstLayerNode(node));
+    //   });
+    //   Promise.all(promiseList).then(values => {
+    //     this.canRender = true;
+    //     console.log(values);
+    //   });
+    // });
+    // axios.get('/example.json')
+    //   .then(response => {
+    //     this.nodes = response.data.nodes
+    //     this.links = response.data.links
+    //     this.canRender = true
+    //   }).catch(err=>console.log(err))
+    //     // 尝试生成豆瓣节点
+    // console.log("正在获取数据");
+    // let nodeData = new Set();
+    // let linkData = new Set();
+    // // let flag = true // 请求数据中
+    // axios
+    //   .get("/douban-graph.json")
+    //   .then(res => {
+    //     console.log(res);
+    //     res.data.forEach(movie => {
+    //       // console.log(item.title)
+    //       nodeData.add({
+    //         name: movie.title,
+    //         type: "movie",
+    //         actors: movie.actor
+    //       });
+    //       let actors = movie.actor;
+    //       for (let i in actors) {
+    //         nodeData.add({
+    //           name: actors[i],
+    //           type: "actor"
+    //         });
+    //       }
+    //     });
+    //     console.log(nodeData);
+    //     this.nodes = Array.from(nodeData);
+    //     this.canRender = true
+    //   })
+    //   .catch(err => {
+    //     console.log(err);
+    //   });
   },
+  methods: {
+    startQuery(query) {
+      this.getFirstLayerNode(query).then(res => {
+        let promiseList = [];
+        res.forEach(node => {
+          promiseList.push(this.getFirstLayerNode(node));
+        });
+        Promise.all(promiseList).then(values => {
+          this.canRender = true;
+          console.log(values);
+        });
+      });
+    },
+
+    getFirstLayerNode(query) {
+      return new Promise((resolve, reject) => {
+        jsonpFetch(
+          "//suggestqueries.google.com/complete/search?client=firefox&q=" +
+            encodeURIComponent(query + "vs")
+        )
+          .then(response => {
+            let linkedNodes = [];
+            // let node1 = response[0].trim();
+            let node1 = query.trim();
+            if (this.nodeIds.indexOf(node1) === -1) {
+              this.nodes.push({
+                id: node1,
+                name: node1
+              });
+              this.nodeIds.push(node1);
+            }
+
+            response[1].forEach(statement => {
+              let node2 = "";
+              // 注：splice 是返回删除的部分
+              statement = statement.split("vs");
+              console.log(statement);
+              if (statement[1]) {
+                node2 = statement[1].trim();
+              }
+              // 如果不为空字符串
+              if (node2 && this.nodeIds.indexOf(node2) === -1) {
+                linkedNodes.push(node2);
+                this.nodes.push({
+                  id: node2,
+                  name: node2
+                });
+                this.nodeIds.push(node2);
+                this.links.push({
+                  source: node1,
+                  target: node2,
+                  value: 1
+                });
+              }
+            });
+            resolve(linkedNodes);
+          })
+          .catch(err => {
+            console.log(err);
+            reject(err);
+          });
+      });
+    }
+  }
 };
 </script>
 
@@ -58,6 +173,6 @@ html {
 }
 #modeSwitch {
   position: absolute;
-  top: 20px
+  top: 20px;
 }
 </style>
--- src/components/Network.vue
+++ src/components/Network.vue
@@ -11,6 +11,7 @@
       :height="svgSize.height"
       :class="svgClass"
       :style="{'background-color': theme.bgcolor}"
+      @click="clickEle"
       @mousedown="svgMousedown"
       @mouseup="svgMouseup"
       @mouseover.prevent="svgMouseover"
@@ -23,7 +24,7 @@
             <line
               :class="`${link.type} ${link.selected} link element`"
               :stroke="theme.linkStroke"
-              stroke-width="2"
+              stroke-width="5"
             ></line>
             <!-- dx dy 文字左下角坐标 -->
             <text
@@ -41,7 +42,7 @@
         <g id="node-group">
           <g v-for="node in nodes" :key="node.index">
             <circle
-              :fill="nodeColor(node.group)"
+              :fill="nodeColor(node.id)"
               stroke-width="3"
               :stroke="theme.nodeStroke"
               :class="`${node.type} ${node.showText?'selected' : ''} node element`"
@@ -49,11 +50,11 @@
             ></circle>
             <text
               v-show="node.showText"
-              dx="13"
+              :dx="nodeSize"
               dy="0"
               class="node-text"
               :fill="theme.textFill"
-              font-size="10"
+              font-size="20"
             >{{node.name}}</text>
           </g>
           <g></g>
@@ -66,6 +67,9 @@
 
 <script>
 import * as d3 from "d3"; // 先这样导入 最后优化的时候要按需导入
+import d3SelectionMulti from "d3-selection-multi";
+
+// const d3 = Object.assign({}, d3Origin, d3SelectionMulti)
 
 let x = 0,
   y = 0,
@@ -77,8 +81,8 @@ DOMTokenList.prototype.indexOf = Array.prototype.indexOf;
 export default {
   name: "network",
   props: {
-    nodes: Array,
-    links: Array,
+    nodeList: Array,
+    linkList: Array,
     showLinkText: Boolean,
     forceSize: {
       type: Object,
@@ -110,13 +114,29 @@ export default {
         nodes: []
       },
       pinned: [], // 被订住的中心节点的下标
-      nodeSize: 10,
-      force: d3.layout.force(),
-      zoom: d3.behavior.zoom(),
-      nodeColor: d3.scale.category20()
+      nodeSize: 15,
+      force: null,
+      zoom: d3.zoom(),
+      nodeColor: d3.scaleOrdinal(d3.schemeCategory10) // d3.scale.category20()
     };
   },
   computed: {
+    //  // 是否渲染节点 - 懒加载
+    // display(x, y) {
+    //   // if (x > 0 && y > 0) {
+    //   //   return true
+    //   // }
+    //   // else {
+    //     console.log(x + ' ' + y)
+    //   //   return false
+    //   // }
+    // },
+    nodes() {
+      return this.nodeList;
+    },
+    links() {
+      return this.linkList;
+    },
     theme() {
       if (this.mode === "light") {
         return {
@@ -136,94 +156,144 @@ export default {
     }
   },
   created() {
-    // 初始化 force 布局
-    this.force
-      .nodes(this.nodes)
-      .links(this.links)
-      .size([this.forceSize.width, this.forceSize.height]) //指定作用域范围
-      .linkDistance(50) //指定连线长度
-      .friction(0.9)
-      .charge([-200]) //相互之间的作用力
-      .gravity(0.2);
+    console.log("开始创建")
 
-    this.force.start(); //开始作用
+    console.log(this.nodes)
 
     let i = 0;
     this.links.forEach(link => {
       link.id = i++;
     });
 
+    // 初始化 force 布局
+
+    // .nodes(this.nodes)
+    // .links(this.links)
+    // .size([this.forceSize.width, this.forceSize.height]) //指定作用域范围
+    // .linkDistance(50) //指定连线长度
+    // .friction(0.9)
+    // .charge([-200]) //相互之间的作用力
+    // .gravity(0.2);
+
+    this.force = d3
+      .forceSimulation(this.nodes)
+      .force("link", d3.forceLink(this.links).id(d => d.id).distance(100))
+      //.force("charge", d3.forceManyBody())
+      // .linkDistance(50) //指定连线长度
+
+      .force("charge", d3.forceManyBody().strength(-100)) //The strength of the attraction or repulsion
+      // .forceCollide(2)
+      .force(
+        "center",
+        d3.forceCenter(this.forceSize.width / 2, this.forceSize.height / 2)
+      )
+      // .alphaDecay(0.5) //设置 alpha 衰减率.迭代150，默认0.0228
+
+    // this.force.start(); //开始作用
+
     console.log(this.nodes);
     console.log(this.links);
 
     // 初始化 zoom
     this.zoom
-      .translate([0, 0]) // 初始位置
-      .scale(1) // 初始比例
+      // .translate([0, 0]) // 初始位置
+      // .scale(1) // 初始比例
       .scaleExtent([0.5, 4])
       .on("zoom", this.zoomed); //??? 直接写在<svg>上了不知是否ok
   },
   mounted() {
     // 给节点添加拖拽
-    d3.selectAll(".node").call(this.force.drag);
+    d3.selectAll(".node").call(this.drag(this.force));
     // 更新坐标 - 更新坐标 - 每一个 tick
-    this.force.on("tick", () => {
-      //更新连线坐标
-      d3.selectAll(".link")
-        .data(this.links)
-        .attr({
-          x1: d => d.source.x,
-          y1: d => d.source.y,
-          x2: d => d.target.x,
-          y2: d => d.target.y
-        });
-      //更新节点坐标
-      d3.selectAll(".node")
-        .data(this.nodes)
-        .attr({
-          cx: d => d.x,
-          cy: d => d.y
-        });
-      // 更新文字坐标
-      d3.selectAll(".node-text")
-        .data(this.nodes)
-        .attr({
-          x: d => d.x,
-          y: d => d.y
-        });
-      d3.selectAll(".link-text")
-        .data(this.links)
-        .attr({
-          x: d => (d.source.x + d.target.x) / 2,
-          y: d => (d.source.y + d.target.y) / 2
-        });
-    });
-    d3.select("svg")
-      .call(this.zoom)
-      .call(this.zoom.event);
+
+    this.force
+      //  .nodes(this.nodes)
+
+      //  .force("link")
+      // .links(this.links)
+      .on("tick", () => {
+        //更新连线坐标
+        d3.selectAll(".link")
+          .data(this.links)
+          .attrs({
+            x1: d => d.source.x,
+            y1: d => d.source.y,
+            x2: d => d.target.x,
+            y2: d => d.target.y
+          });
+
+        //       //  link
+        //     .attr("x1", d => d.source.x)
+        //     .attr("y1", d => d.source.y)
+        //     .attr("x2", d => d.target.x)
+        //     .attr("y2", d => d.target.y);
+
+        // // node
+
+        //更新节点坐标
+        d3.selectAll(".node")
+          .data(this.nodes)
+          .attrs({
+            cx: d => d.x,
+            cy: d => d.y
+          });
+        // 更新文字坐标
+        d3.selectAll(".node-text")
+          .data(this.nodes)
+          .attrs({
+            x: d => d.x,
+            y: d => d.y
+          });
+        d3.selectAll(".link-text")
+          .data(this.links)
+          .attrs({
+            x: d => (d.source.x + d.target.x) / 2,
+            y: d => (d.source.y + d.target.y) / 2
+          });
+      });
+
+    d3.select("svg").call(this.zoom);
+    // .call(this.zoom.event);
   },
   methods: {
-    svgMousedown(e) {
-      // 如果是按在节点上的
-      if (e.target.tagName === "circle") {
+    clickLink(e) {
+      console.log("嘿，点击边了")
+      let link  = e.target.__data__
+      console.log(link.source.name + ' vs ' + link.target.name)
+      this.showDetails(link.source.name + ' vs ' + link.target.name)
+    },
+    clickNode(e) {
+      // if (e.target.tagName === "circle") {
         // 阻止鼠标按下事件向下传播 -> 防止触发 zoom 的拖拽
         e.stopImmediatePropagation();
         if (this.pinned.length === 0) {
           this.pinnedState(e);
         } else {
-          d3.selectAll(".element").style({
+          d3.selectAll(".element").styles({
             opacity: 0.2
           });
           this.pinned = [];
         }
-      } else {
-        // 改变鼠标样式
-        this.svgClass.mouseup = false;
-        this.svgClass.mousedown = true;
+      // }
+    },
+    clickEle(e) {
+      if (e.target.tagName === "circle") {
+        this.clickNode(e)
       }
+      else if (e.target.tagName === "line") {
+        this.clickLink(e)
+      }
+    },
+    svgMousedown(e) {
+      // 这个监听器只能监听到非节点的内容
+      // 改变鼠标样式
+      console.log('鼠标按下')
+      this.svgClass.mouseup = false;
+      this.svgClass.mousedown = true;
     },
     svgMouseup() {
       // 改变鼠标样式
+      console.log("鼠标放")
       this.svgClass.mouseup = true;
       this.svgClass.mousedown = false;
     },
@@ -234,6 +304,13 @@ export default {
         }
         // 强制刷新
         this.$forceUpdate();
+      }
+      else if (e.target.nodeName === "line") {
+        console.log("嘿 hover 边")
+        console.log("坐标：")
+        console.log(e.clientX + ', ' +e.clientY)
+        let link  = e.target.__data__
+      console.log(link.source.name + ' vs ' + link.target.name)
       }
     },
     svgMouseout(e) {
@@ -253,7 +330,7 @@ export default {
       // 周围节点显示文字、边和结点增加 selected class、添加进 selection
       this.lightNeibor(e.target.__data__);
       // 除了 selected 的其余节点透明度减小
-      d3.selectAll(".element").style({
+      d3.selectAll(".element").styles({
         opacity: 0.2
       });
     },
@@ -264,13 +341,13 @@ export default {
       // 周围节点不显示文字、边和结点移除 selected class
       this.darkenNerbor();
       // 所有节点透明度恢复
-      d3.selectAll(".element").style({
+      d3.selectAll(".element").styles({
         opacity: 1
       });
     },
     pinnedState(e) {
       this.pinned.push(e.target.__data__.index);
-      d3.selectAll(".element").style({
+      d3.selectAll(".element").styles({
         opacity: 0.05
       });
     },
@@ -316,16 +393,50 @@ export default {
       this.selection.nodes = [];
       this.selection.links = [];
     },
+    showDetails(query) {
+      // const query = resolveQueryFromLink(link.fromId, link.toId);
+      window.open('https://google.com/search?q=' + encodeURIComponent(query), '_blank');
+    },
     zoomed() {
       // 缩放中：以鼠标所在的位置为中心
-      // console.log(d3.event);
-      x = d3.event.translate[0];
-      y = d3.event.translate[1];
-      s = d3.event.scale;
+      console.log(d3.event);
+      x = d3.event.transform.x;
+      y = d3.event.transform.y;
+      s = d3.event.transform.k;
       d3.select("#container").attr(
         "transform",
-        "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"
+        "translate(" +
+          d3.event.transform.x +
+          "," +
+          d3.event.transform.y +
+          ") scale(" +
+          d3.event.transform.k +
+          ")"
       );
+    },
+    drag(simulation) {
+      function dragstarted(d) {
+        if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
+      }
+
+      function dragged(d) {
+        d.fx = d3.event.x;
+        d.fy = d3.event.y;
+      }
+
+      function dragended(d) {
+        if (!d3.event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+      }
+
+      return d3
+        .drag()
+        .on("start", dragstarted)
+        .on("drag", dragged)
+        .on("end", dragended);
     },
     reset() {
       let zoom = this.zoom;
@@ -336,7 +447,9 @@ export default {
           let xi = d3.interpolate(x, 0);
           let yi = d3.interpolate(y, 0);
           return function(t) {
+            console.log(zoom);
             d3.select("svg").call(
+              // zoom.transform([si(t),xi(t),yi(t)])
               zoom.translate([xi(t), yi(t)]).scale(si(t)).event
             );
           };
@@ -378,7 +491,7 @@ svg.mousedown {
   /* fill: "gray";
   font-size: 10; */
 }
-.node {
+.node, .link {
   /* fill: "darkcyan"; 在 css 里使用不如在代码中使用权重高 */
   cursor: pointer;
 }
